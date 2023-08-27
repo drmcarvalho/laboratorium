23e0c4b035232641df97c2c8ce1a7b82aa61793f
drmcarvalho <furiano.k@gmail.com>
2023-08-26 23:18:35 -0300

    controller listar testes

---
+++ src/main/java/com/example/rest/laboratorium/controllers/testes/ListarTodosTestesController.java
@@ -0,0 +1,7 @@
+package com.example.rest.laboratorium.controllers.testes;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ListarTodosTestesController {
+}
