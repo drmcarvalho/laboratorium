8253b3e9760a6966fca51fc6519a9f2b86d5aacf
drmcarvalho <furiano.k@gmail.com>
2023-08-29 01:39:42 -0300

    update teste

---
+++ src/main/java/com/example/rest/laboratorium/controllers/testes/AtualizarTesteController.java
@@ -0,0 +1,55 @@
+package com.example.rest.laboratorium.controllers.testes;
+
+import com.example.rest.laboratorium.commons.resposta.GerarRespostaBadRequest;
+import com.example.rest.laboratorium.commons.validador.CampoVazio;
+import com.example.rest.laboratorium.models.Teste;
+import com.example.rest.laboratorium.models.TesteModelAssembler;
+import com.example.rest.laboratorium.repositories.cenario.CenarioRepositorio;
+import com.example.rest.laboratorium.repositories.teste.TesteRepositorio;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SuppressWarnings({"unused", "ClassCanBeRecord"})
+@RestController
+public class AtualizarTesteController {
+    private final TesteRepositorio testeRepositorio;
+    private final CenarioRepositorio cenarioRepositorio;
+    private final TesteModelAssembler modelAssembler;
+
+    public AtualizarTesteController(
+            TesteRepositorio testeRepositorio,
+            CenarioRepositorio cenarioRepositorio,
+            TesteModelAssembler modelAssembler) {
+        this.testeRepositorio = testeRepositorio;
+        this.cenarioRepositorio = cenarioRepositorio;
+        this.modelAssembler = modelAssembler;
+    }
+
+    public ResponseEntity<?> action(@RequestBody Teste teste, @PathVariable Long id) {
+        if (CampoVazio.validar(teste.getTitulo())) {
+            return GerarRespostaBadRequest.resposta("Error de campo", "Forneça um titulo para o teste");
+        }
+        if (CampoVazio.validar(teste.getDescricao())) {
+            return GerarRespostaBadRequest.resposta("Error de campo", "Forneça uma descrição para o teste");
+        }
+        if (!cenarioRepositorio.findById(teste.getIdCenario()).isPresent())
+            return GerarRespostaBadRequest.resposta("Error de campo", "O cenario id especificado nao existe");
+
+
+        return ResponseEntity.ok(testeRepositorio.findById(id)
+                .map(testeAlteracao -> {
+                    testeAlteracao.setTesteSituacao(teste.getTesteSituacao());
+                    testeAlteracao.setTitulo(teste.getTitulo());
+                    testeAlteracao.setDescricao(teste.getDescricao());
+                    testeAlteracao.setIdCenario(teste.getIdCenario());
+                    return modelAssembler.toModel(testeRepositorio.save(teste));
+                })
+                .orElseGet(() -> {
+                    teste.setId(id);
+                    return modelAssembler.toModel(testeRepositorio.save(teste));
+                })
+        );
+    }
+}
--- src/main/java/com/example/rest/laboratorium/controllers/testes/ListarTodosTestesController.java
+++ src/main/java/com/example/rest/laboratorium/controllers/testes/ListarTodosTestesController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.stream.Collectors;
 
+@SuppressWarnings({"unused", "ClassCanBeRecord"})
 @RestController
 public class ListarTodosTestesController {
     private final TesteRepositorio repositorio;
