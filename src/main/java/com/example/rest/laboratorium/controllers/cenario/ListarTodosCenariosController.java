6db5b18c6e0c96c5e8341f49ccd9433704d8f0d5
drmcarvalho <furiano.k@gmail.com>
2023-09-05 14:53:45 -0300

    refac

--- src/main/java/com/example/rest/laboratorium/controllers/cenario/ListarTodosCenariosController.java
+++ src/main/java/com/example/rest/laboratorium/controllers/cenario/ListarTodosCenariosController.java
@@ -1,19 +1,14 @@
 package com.example.rest.laboratorium.controllers.cenario;
 
-import com.example.rest.laboratorium.models.Cenario;
 import com.example.rest.laboratorium.models.CenarioModelAssembler;
 import com.example.rest.laboratorium.repositories.CenarioRepositorio;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.tags.Tags;
 import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.stream.Collectors;
 
 @SuppressWarnings({"unused", "ClassCanBeRecord"})
 @RestController
